e71eda2c12e96701ebb937db3782e367287e68dc
Herpaderp Aldent <felix.a.huber@gmx.net>
2019-08-10 12:17:46 +0200

    Optimize webpack and laravel mix to use `npm run watch-poll`

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,6 +10,7 @@ window._ = require('lodash');
 try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
+    window.Swal = require('sweetalert2');
 
     require('bootstrap');
     require('admin-lte');
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix');
 
+require('laravel-mix-copy-watched');
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -10,10 +12,12 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
-
 if (! mix.inProduction()) {
-  mix.copyDirectory('packages/seatplus/web/src/resources/js/components', './resources/js/components')
+  mix.js('resources/js/app.js', 'public/js')
+      .sass('resources/sass/app.scss', 'public/css')
+      //.copyWatched('packages/seatplus/web/src/resources/js/components/*.{vue}', 'resources/js/components');
+      .copyDirectoryWatched('packages/seatplus/web/src/resources/js/components/', 'resources/js/components');
+  //mix.copyDirectory('packages/seatplus/web/src/resources/js/components', './resources/js/components')
 }
+
+
