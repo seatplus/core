551ff7a7ad68edf0167243eea6872ec9db7428b1
Felix Huber <felix.a.huber@gmx.net>
2022-08-31 22:02:06 +0200

    support vite instead of webpack/mix

---
+++ vite.config.js
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import copy from 'rollup-plugin-copy';
+
+export default defineConfig(({mode}) => {
+    return {
+        server: {
+            cors: mode === "development",
+            watch: {
+                ignored: [
+                    '**/node_modules/**',
+                    '**/vendor/**',
+                    '**/public/**',
+                    '!**/vendor/seatplus/web/**'
+                ],
+            }
+        },
+        plugins: [
+            copy({
+                targets: [{
+                    src: 'vendor/seatplus/web/resources/js',
+                    dest: 'resources'
+                }],
+                //flatten: false,
+                verbose: true,
+                overwrite: true
+            }),
+            laravel({
+                input: 'resources/js/app.js',
+                refresh: ['resources/js/**', 'vendor/seatplus/web/resources/js/**'],
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': '/resources/js',
+            },
+        },
+    }
+});
